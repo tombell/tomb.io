d525b52a2cb009da2a563fa846f6e545b74c959c
Tom Bell <tomb@tombell.dev>
2026-09-20 21:29:26 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,24 +3,19 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, ... }:
-    let
-      systems = [
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-    in
-    {
-      devShells = nixpkgs.lib.genAttrs systems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [ pkgs.hugo ];
-          };
-        });
-    };
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+  in {
+    devShells = nixpkgs.lib.genAttrs systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = [pkgs.hugo];
+      };
+    });
+  };
 }
